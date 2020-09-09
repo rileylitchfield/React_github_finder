dee041a79ebcfd93d095124c0dc2ebf28b5bcd13
Riley <rileylitchfield95@gmail.com>
2020-09-08 20:51:32 -0500

    Completed http requests and updating state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { Component } from "react";
-import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
-import "./App.css";
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
+import './App.css';
 
-class App extends React.Component {
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+    console.log(res.data);
+  }
+
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Navbar />
-        <div className='container'>
-          <Users />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
